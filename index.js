36dd1b809aec3d4fc19e0e73023816c0864f1fa2
James Ryan Nielcen <rnielcen@hotmail.com>
2018-01-14 07:44:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ mixedCasse.toLowerCase() === mixedCase
 
 mixedCase.toUpperCase() === mixedCase 
 
-function sayHiToGrandma('hello') {
-  if(lowerCase)
+function sayHiToGrandma(string) {
+  if(string.toLowerCase())
   return "I can't hear you!"
 }
 
