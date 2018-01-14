cfe996dbf5fe741248e8353bbc0fa3f0df0d7336
James Ryan Nielcen <rnielcen@hotmail.com>
2018-01-14 08:23:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function sayHiToGrandma(string) {
 return "I can't hear you!"
  else if(string.toUpperCase() === string)
 return "YES INDEED!"
-else if(string.mixedCase()) === 
-
+else if(string === "I you, Grandma!")
+return "I love you, too."
+}
 
